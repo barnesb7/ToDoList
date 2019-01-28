6dc2916e2d920027051baf0c4533477e3e5bb982
Brian B <brianb@MacBook-Pro-4.local>
2019-01-28 16:23:12 -0500

    Able to take user input and exit program

--- src/com/company/ToDoListDemo.java
+++ src/com/company/ToDoListDemo.java
@@ -1,11 +1,38 @@
 package com.company;
+import java.util.Scanner;
 
 public class ToDoListDemo {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         UserMenu userMenu = new UserMenu();
 
-        userMenu.displayMenu();
+        boolean shouldContinue = true;
+        String userInput;
+
+        do{
+            userMenu.displayMenu();
+            userInput = scanner.nextLine();
+
+            if(userInput.equals("1")){
+
+            } else if (userInput.equals("2")){
+
+            } else if (userInput.equals("3")){
+
+            } else if (userInput.equals("4")){
+
+            } else if (userInput.equals("5")){
+
+            } else if (userInput.equals("6")){
+                shouldContinue = false;
+                System.out.println("Thank you! Goodbye");
+            } else {
+                System.out.println("Please enter one of the below options by number");
+            }
+
+        } while (shouldContinue);
     }
 }
